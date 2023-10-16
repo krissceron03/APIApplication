9ec5f818a8a953a77b1059216968f32640e1e085
camil <camil@LAPTOPCAMI>
2023-10-16 07:17:35 -0500

    Agregar archivos de proyecto.

---
+++ APIProductos/Data/ApplicationDBContext.cs
@@ -0,0 +1,14 @@
+﻿using APIProductos.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace APIProductos.Data
+{
+    public class ApplicationDBContext : DbContext
+    {
+        public ApplicationDBContext(
+           
+            DbContextOptions<ApplicationDBContext> options ) : base( options ) { }
+            
+        DbSet<Producto> Productos { get; set; }
+    }
+}
